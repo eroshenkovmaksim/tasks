17686718a94c8b620da01b213f3e6a121e6419cb
maksim.eroshenkov <maksim.eroshenkov@netcracker.com>
2019-08-14 19:09:45 +0300

    Initial commit

---
+++ src/main/java/eroshenkov/check_message/MessageChecker.java
@@ -0,0 +1,9 @@
+package main.java.eroshenkov.check_message;
+
+public class MessageChecker {
+    public static void main(String[]args){
+        String text = "qwerty ght lsd keks oaos ok, qws. ght";
+        String message = "lsd ght";
+
+    }
+}
---
+++ src/main/java/eroshenkov/product/Product.java
@@ -0,0 +1,37 @@
+package main.java.eroshenkov.product;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Product {
+  public static void main(String[] args) {
+    int n = 5;
+    int[] inputArray = new int[n];
+    int[] outputArray = new int[n];
+    int[] leftProduct = new int[n - 2];
+    int[] rightProduct = new int[n - 2];
+    Random rand = new Random();
+    for (int i = 0; i < n; i++) {
+      inputArray[i] = rand.nextInt(10);
+    }
+    int lProduct = 1;
+    int rProduct = 1;
+    for (int i = 0; i < n - 1; i++) {
+      lProduct *= inputArray[i];
+      rProduct *= inputArray[n - i - 1];
+      if (i == n - 2) {
+        break;
+      }
+      leftProduct[i] = lProduct;
+      rightProduct[n - i - 3] = rProduct;
+    }
+    outputArray[0] = rProduct;
+    outputArray[n - 1] = lProduct;
+    for (int i = 0; i < n - 2; i++) {
+      outputArray[i + 1] = leftProduct[i] * rightProduct[i];
+    }
+    Arrays.stream(inputArray).boxed().forEach(element -> System.out.print(element + " "));
+    System.out.println();
+    Arrays.stream(outputArray).boxed().forEach(element -> System.out.print(element + " "));
+  }
+}
